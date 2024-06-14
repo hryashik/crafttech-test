5cd40a2650190f81a73e95f60627b98cd87b0dd5
hryashik <tsukikohaya@gmail.com>
2024-06-14 12:23:20 +0300

    create test environment, write e2e tests

--- src/middlewares/exceptionFilter.ts
+++ src/middlewares/exceptionFilter.ts
@@ -5,6 +5,7 @@ function exceptionFilter(err: Error, req: Request, res: Response) {
   if (err instanceof HttpException) {
     res.status(err.statusCode).send({ error: err.message });
   } else {
+    console.error(err);
     res.status(500).send({ error: "Internal server error" });
   }
 }
--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -1,16 +1,120 @@
 import app from "../src/main";
 import request from "supertest";
 
+let taskId: number;
+
 describe("Tasks", () => {
+  describe("/tasks @POST", () => {
+    describe("Incorrect data", () => {
+      it("Should return 400", async () => {
+        const res1 = await request(app)
+          .post("/tasks")
+          .send({ description: "asd" });
+        const res2 = await request(app)
+          .post("/tasks")
+          .send({ description: "asd", status: "in_process" });
+        const res3 = await request(app).post("/tasks").send({ title: "as" });
+        expect(res1.statusCode).toBe(400);
+        expect(res2.statusCode).toBe(400);
+        expect(res3.statusCode).toBe(400);
+      });
+    });
+    describe("Correct data", () => {
+      it("Should return 201", async () => {
+        const res = await request(app)
+          .post("/tasks")
+          .send({ title: "first task", description: "some" });
+
+        taskId = res.body.id;
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toHaveProperty("id");
+      });
+    });
+  });
   describe("/tasks @GET", () => {
-    it("Should return 200", async () => {
-      const res = await request(app).get("/tasks");
-      expect(res.statusCode).toBe(200);
+    describe("Correct", () => {
+      it("Should return 200", async () => {
+        const res = await request(app).get("/tasks");
+        expect(res.statusCode).toBe(200);
+        expect(res.body.length).toBe(1);
+      });
+    });
+  });
+  describe("/tasks/:id @GET", () => {
+    describe("Incorrect id", () => {
+      it("Should return 400", async () => {
+        const res = await request(app).get("/tasks/asda");
+        expect(res.statusCode).toBe(400);
+      });
+    });
+    describe("Id not found", () => {
+      it("Should return 404", async () => {
+        const res = await request(app).get("/tasks/999");
+        expect(res.statusCode).toBe(404);
+      });
+    });
+    describe("correct id", () => {
+      it("Should return 200", async () => {
+        const res = await request(app).get(`/tasks/${taskId}`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.id).toBe(taskId);
+      });
+    });
+  });
+  describe("/tasks/:id @PUT", () => {
+    describe("Incorrect id", () => {
+      it("Should return 400", async () => {
+        const res = await request(app)
+          .put("/tasks/asda")
+          .send({ title: "updated task" });
+        expect(res.statusCode).toBe(400);
+      });
+    });
+    describe("Id not found", () => {
+      it("Should return 404", async () => {
+        const res = await request(app)
+          .put("/tasks/999")
+          .send({ title: "updated task" });
+        expect(res.statusCode).toBe(404);
+      });
+    });
+    describe("correct id", () => {
+      it("Should return 200", async () => {
+        const res = await request(app)
+          .put(`/tasks/${taskId}`)
+          .send({
+            title: "updated task",
+            description: "some description",
+            status: "completed",
+          });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.id).toBe(taskId);
+      });
+    });
+  });
+  describe("/tasks/:id @GET", () => {
+    describe("Incorrect id", () => {
+      it("Should return 400", async () => {
+        const res = await request(app).delete("/tasks/asda");
+        expect(res.statusCode).toBe(400);
+      });
+    });
+    describe("Id not found", () => {
+      it("Should return 404", async () => {
+        const res = await request(app).delete("/tasks/999");
+        expect(res.statusCode).toBe(404);
+      });
+    });
+    describe("correct id", () => {
+      it("Should return 204", async () => {
+        const res = await request(app).delete(`/tasks/${taskId}`);
+        expect(res.statusCode).toBe(204);
+        expect(res.body.id).toBe(taskId);
+      });
     });
   });
 });
 
 afterAll(() => {
-  app.unref();
   app.close();
-})
+});
